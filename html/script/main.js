9f5ec53e08ab5ac913a439d91af0458d986c6027
Thomas <tjb47.dev@gmail.com>
2023-07-14 11:28:01 -0400

    recreated repo for new loading screen

---
+++ html/script/main.js
@@ -0,0 +1,92 @@
+let audio = new Audio();
+var request = new XMLHttpRequest();
+var count = 0;
+function Main(){
+    
+    return{
+        DiscordGuildId: '923488921906724894', // Also know as Discord server ID
+        DiscordInviteLink: 'https://discord.gg/elementrp18',
+        memberCount: 0,
+        musicAutoplay: true, // Set this to true if you want the music to autoplay
+        musicVolume: 0.1, // Set the volume that you like (0 = 0% ; 0.5 = 50% ; 1 = 100%)
+        buttons:[
+            {label: 'Home', selected: true},
+            {label: 'News', selected: false},
+            {label: 'Team', selected: false},
+        ],
+        musicList: [
+            {label: 'Hold up Hold up',author: 'Young Dolph',src: 'audio/hold-up.mp3'},
+        ],
+        team:[
+            {discord: 'Ace Huli', role: 'Head of State', img: 'img/ace.png'},
+            {discord: 'CoBRa47', role: 'Head of State', img: 'img/cobra.png'},
+            {discord: 'ledman3', role: 'Head of State', img: 'img/ledman.jpg'},
+            {discord: 'd3xter', role: 'Admin / Staff', img: 'img/dexter.gif'},
+        ],
+        // No touching here!!!!
+        isMusicPlaying: false,
+        musicOpen: false,
+        currentSong: 0,
+        listen(){
+            if(this.musicAutoplay){
+                setTimeout(() => { this.play();}, 100);
+            }
+            request.open('GET', 'https://discordapp.com/api/guilds/'+this.DiscordGuildId+'/widget.json', true);
+            request.onload = function() {
+            if (request.status >= 200 && request.status < 400) {
+                var data = JSON.parse(request.responseText);
+                count = data.presence_count;
+            }
+            };    
+            request.onerror = function() {
+            };
+            request.send();   
+            setTimeout(() => { this.memberCount = count; }, 1000);
+        },
+        selectBtn(select){
+            this.buttons.forEach(function(button){
+                button.selected = false;
+            });
+            return true;
+        },
+        play(){
+            audio.src = this.musicList[this.currentSong].src;
+            audio.load();
+            audio.play();
+            audio.volume = this.musicVolume;
+            this.isMusicPlaying = true;
+        },
+        pause(){
+            audio.pause()
+            this.isMusicPlaying = false;
+        },
+        next(){
+            if(this.isMusicPlaying){
+                audio.pause()
+            }
+            if(this.currentSong < this.musicList.length-1){
+                this.currentSong++;
+            }else{
+                this.currentSong = 0;
+            }
+            audio.src = this.musicList[this.currentSong].src;
+            audio.load();
+            audio.play();
+            this.isMusicPlaying = true;
+        },
+        prev(){
+            if(this.isMusicPlaying){
+                audio.pause()
+            }
+            if(this.currentSong != 0){
+                this.currentSong = this.currentSong-1;
+            }else{
+                this.currentSong = this.musicList.length-1;
+            }
+            audio.src = this.musicList[this.currentSong].src;
+            audio.load();
+            audio.play();
+            this.isMusicPlaying = true;
+        },
+    }
+}
